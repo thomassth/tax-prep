d62fed8378d57c312ba9c82794ac64bb406459bc
Thomas Kam <thomassth@users.noreply.github.com>
2024-01-25 02:57:54 -0500

    persist input

--- src/stores/input.ts
+++ src/stores/input.ts
@@ -1,20 +1,34 @@
-import { ref, computed } from 'vue'
+import { computed } from 'vue'
 import { defineStore } from 'pinia'
+import { useLocalStorage } from '@vueuse/core'
 
 export const useInputStore = defineStore('input', () => {
-  const input = ref(new Map())
+  const categories = [
+    { id: 'study', heading: '📚 studied' },
+    { id: 'work', heading: '📊 worked' },
+    { id: 'work_self', heading: '🔧 & is self employed', under: 'work' },
+    { id: 'work_union', heading: '💪 & have a union', under: 'work' },
+    { id: 'newcomer', heading: '🇨🇦 first year in Canada' },
+    { id: 'healthcare', heading: '💊 Spent money on healthcare' },
+    { id: 'rent', heading: '🛏️ Paid rent' },
+    { id: 'children', heading: '👶 Spent money on children' },
+    { id: 'donation', heading: '😇 donated money' }
+  ]
+  const input = useLocalStorage('input', new Map())
   const selected = computed(() => [...input.value.keys()])
   const printInput = computed(() => JSON.stringify([...input.value.keys()]))
-  // function increment() {
-  //   count.value++
-  // }
   const checkboxChange = (string: string) => {
     if (input.value.has(string)) {
       input.value.delete(string)
+      categories.forEach((category) => {
+        if (category.under === string) {
+          input.value.delete(category.id)
+        }
+      })
     } else {
       input.value.set(string, true)
     }
   }
 
-  return { input, selected, printInput, checkboxChange }
+  return { categories, input, selected, printInput, checkboxChange }
 })
