47126c5ecde1e0ac9c616d4e6cb988b9a36c0294
Thomas Kam <thomassth@users.noreply.github.com>
2024-02-25 02:31:24 -0500

    section descriptions

--- src/components/EssentialItems.vue
+++ src/components/EssentialItems.vue
@@ -17,7 +17,10 @@ const filteredItems = computed(() =>
 
 <template>
   <fieldset class="essentials">
-    <h1>Essential stuff</h1>
+    <legend>
+      <h1>Essential information</h1>
+      <p>These are used to fill in basic details of the tax return.</p>
+    </legend>
     <div class="output">
       <template v-for="item in filteredItems" :key="item.id">
         <ChecklistItem :id="item.id" :onChange="outputStore.checkboxChange"
@@ -36,5 +39,12 @@ const filteredItems = computed(() =>
 .essentials {
   border: 1px solid yellow;
   margin-bottom: 1rem;
+}
+
+legend {
+  background: rgba(128, 128, 128, 0.2);
+  padding: 1rem;
+  margin: 1rem;
+  border-radius: 4px;
 }
 </style>
